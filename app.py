81963db3405b29f417cf2093ab058bfa3d208fb9
Monica Surtani <142895627+Monica-Surtani@users.noreply.github.com>
2026-04-22 18:30:27 +0530

    indent issues resolved

--- app.py
+++ app.py
@@ -227,13 +227,16 @@ if email and email in participants_df["email"].values:
     # client = gspread.authorize(creds)
     # return client.open("ANNOTATION_DATA")
     def get_gsheet():
-    creds = Credentials.from_service_account_info(
-        st.secrets["gcp_service_account"],
-        scopes=[
-            "https://www.googleapis.com/auth/spreadsheets",
-            "https://www.googleapis.com/auth/drive"
-        ],
-    )
+        creds = Credentials.from_service_account_info(
+            st.secrets["gcp_service_account"],
+            scopes=[
+                "https://www.googleapis.com/auth/spreadsheets",
+                "https://www.googleapis.com/auth/drive"
+            ],
+        )
+
+    client = gspread.authorize(creds)
+    return client.open("ANNOTATION_DATA")
 
     client = gspread.authorize(creds)
     return client.open("ANNOTATION_DATA")
